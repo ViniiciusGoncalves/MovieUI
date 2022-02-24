8ed378ffa1683081201d56f7614037c623c41a2e
Vinicius <vg76451@gmail.com>
2022-02-24 10:35:13 -0300

    Initial Commit

---
+++ MovieUI/MovieUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieUIApp.swift
+//  MovieUI
+//
+//  Created by Joao Victor on 24/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
